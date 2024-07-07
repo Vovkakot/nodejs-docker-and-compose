da28f6f9a98abe52ae0fd345c19e5e43f3e7bfad
Vovkakot <vovakot332@gmail.com>
2024-07-07 20:58:13 +0300

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,16 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:8081',
-      'http://katasonov.kpd.nomorepartiesco.ru',
-      'https://katasonov.kpd.nomorepartiesco.ru',
-    ],
-    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-    credentials: true,
-  });
-
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:8081',
+  //     'http://katasonov.kpd.nomorepartiesco.ru',
+  //     'https://katasonov.kpd.nomorepartiesco.ru',
+  //   ],
+  //   app.enableCors();
+  //   methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+  //   credentials: true,
+  // });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
